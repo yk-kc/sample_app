97aa7b56f731a0ad0f48deb78874d90c8d24ceba
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-03-07 21:03:09 +0000

    [Finish]3-9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,14 +2,14 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
-    list = List.new(list_params)
-    if list.save
+    @list = List.new(list_params)
+    if @list.save
     redirect_to list_path(@list.id)
   else
-    render :index
-    
+    render :new
+  end
   end
 
   def index
@@ -23,19 +23,19 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
     redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
-  
+
   private
   # ストロングパラメータ
   def list_params
