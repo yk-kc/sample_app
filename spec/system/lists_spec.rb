5b174c17d6bc76e903ff2549d9ccae79201169de
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-03-31 14:32:01 +0000

    Rspecテスト記述　詳細画面のテストまで

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -29,6 +29,7 @@ describe '投稿のテスト' do
       it '投稿ボタンが表示されているか' do
         expect(page).to have_button '投稿'
       end
+    end
     context '投稿処理のテスト' do
       it '投稿後のリダイレクト先は正しいか' do
         fill_in 'list[title]', with: Faker::Lorem.characters(number:10)
@@ -47,6 +48,32 @@ describe '投稿のテスト' do
       it '一覧表示画面に投稿されたものが表示されているか' do
         expect(page).to have_content list.title
         expect(page).to have_link list.title
+      end
+    end
+  end
+  
+  describe '詳細画面のテスト' do
+    before do
+      visit list_path(list)
+    end
+    context '表示の確認' do
+      it '削除リンクが存在しているか' do
+        expect(page).to have_link '削除'
+      end
+      it '編集リンクが存在しているか' do
+        expect(page).to have_link '編集'
+      end
+    end
+    context 'リンクの遷移先の確認' do
+      it '編集の遷移先は編集画面か' do
+        edit_link = find_all('a')[3]
+        edit_link.click
+        expect(current_path).to eq('/lists/' + list.id.to_s + '/edit')
+      end
+    end
+    context 'list削除のテスト' do
+      it 'listの削除' do
+        expect { list.destroy }.to change{ list.count }.by(-1)
       end
     end
   end
